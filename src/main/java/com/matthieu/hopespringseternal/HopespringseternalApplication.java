7e6b170acb564ab2617e10c63edf997fd0605146
matt <mrmatthieufernandez@gmail.com>
2022-08-24 15:58:54 -0400

    test commit

--- src/main/java/com/matthieu/hopespringseternal/HopespringseternalApplication.java
+++ src/main/java/com/matthieu/hopespringseternal/HopespringseternalApplication.java
@@ -24,7 +24,7 @@ public class HopespringseternalApplication {
 		return List.of(
 				new Student(
 						1L,
-						"Joseph",
+						"Gregor",
 						"joseph.kay@hotmail.com",
 						LocalDate.of(2000, Month.JANUARY, 11),
 						21
