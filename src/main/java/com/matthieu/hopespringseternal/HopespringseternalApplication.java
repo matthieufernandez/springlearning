7b32dc13c1e5505e0f2170c6f02d2d1d5c0b2083
matt <mrmatthieufernandez@gmail.com>
2022-08-24 15:57:32 -0400

    this is a new commit

---
+++ src/main/java/com/matthieu/hopespringseternal/HopespringseternalApplication.java
@@ -0,0 +1,34 @@
+package com.matthieu.hopespringseternal;
+
+import com.matthieu.hopespringseternal.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class HopespringseternalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HopespringseternalApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Joseph",
+						"joseph.kay@hotmail.com",
+						LocalDate.of(2000, Month.JANUARY, 11),
+						21
+				)
+		);
+	}
+}
---
+++ src/test/java/com/matthieu/hopespringseternal/HopespringseternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthieu.hopespringseternal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopespringseternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
