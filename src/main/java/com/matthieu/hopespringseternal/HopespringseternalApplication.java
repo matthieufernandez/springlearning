ffb38ed0583c8ad49a16f3efe368980732d4636b
matt <mrmatthieufernandez@gmail.com>
2022-08-26 12:07:52 -0400

    hooked up JpaRepository

--- src/main/java/com/matthieu/hopespringseternal/HopespringseternalApplication.java
+++ src/main/java/com/matthieu/hopespringseternal/HopespringseternalApplication.java
@@ -1,6 +1,5 @@
 package com.matthieu.hopespringseternal;
 
-import com.matthieu.hopespringseternal.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HopespringseternalApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HopespringseternalApplication.class, args);
 	}
 }
--- src/main/java/com/matthieu/hopespringseternal/student/StudentService.java
+++ src/main/java/com/matthieu/hopespringseternal/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.matthieu.hopespringseternal.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,16 +9,17 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Joseph",
-                        "joseph.kay@hotmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 11),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
 
